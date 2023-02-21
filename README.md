63bf7839a9704563964efbd988de1000a86b685d
David Ramos <davidramos015@gmail.com>
2023-02-20 21:30:35 -0600

    Project setup

---
+++ README.md
@@ -0,0 +1,28 @@
+# Merkle Tree in Blockchain
+
+Get a block hash from the Bitcoin blockchain and build a Merkle Tree from it.
+
+<div
+align="center"
+>
+<img 
+src="./public/merkle-tree-logo.png"
+alt="merkle-tree"
+style="width: 300px; height: 300px; background-color: rgba(230,93,292,0.2)
+; border-radius: 50%;"
+/>
+
+<hr />
+<img 
+src="./public/merkle-tree.webp"
+alt="merkle-tree"
+style="width: 512px; height: 225px; background-color: rgba(230,93,292,0.2)
+;"
+/>
+</div>
+
+# Built with
+1. [Vite](https://vite.org/)
+2. [React](https://reactjs.org/)
+3. [TypeScript](https://www.typescriptlang.org/)
+4. [TailwindCSS](https://tailwindcss.com/)
---
+++ src/helper.ts
@@ -0,0 +1,27 @@
+
+import slugify from "slugify";
+
+export function slugifyText(value: string) {
+
+    return slugify(value, {
+        replacement: '-',  // replace spaces with replacement character, defaults to `-`
+        remove: undefined, // remove characters that match regex, defaults to `undefined`
+        lower: false,      // convert to lower case, defaults to `false`
+        strict: false,     // strip special characters except replacement, defaults to `false`
+        locale: 'vi',      // language code of the locale to use
+        trim: true        // trim leading and trailing replacement chars, defaults to `true`        
+    })
+}
+
+export function generateUUID() {
+
+    let d = new Date().getTime();
+
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+
+    return uuid;
+}
